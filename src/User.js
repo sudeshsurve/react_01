1cfaf6730ed03b27806978c4248106ace3dcc75c
SudesSur <sudesh.surve@npstx.com>
2023-10-12 18:17:44 +0530

    intial comit

---
+++ src/User.js
@@ -0,0 +1,9 @@
+function User(props) {  
+    return (
+      <div className="App">
+        <h2>{props.text}</h2>
+      </div>
+    );
+  }
+
+  export default User
---
+++ src/apple.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function Apple() {
+    const [data, setData] = useState(0)
+    let [show, showDta] = useState(true)
+    const updateState = () => {
+        setData(data + 1)
+    }
+
+    const toggle = () => {
+        showDta(show = !show)
+    }   
+
+    return (
+        <div>
+            {show ? <h1>{data}</h1> : null}
+            <button onClick={updateState}>update</button>
+            <button onClick={toggle}>toggel</button>
+        </div>
+    )
+}
+
+export default Apple
